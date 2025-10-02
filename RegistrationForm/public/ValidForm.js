92d6777523eae644c0a4bfab8ce0d00013a58c8e
ganeshjadhav9361 <ganeshsjadhav3112@gmail.com>
2025-10-02 15:34:55 +0530

    Add Case study Registration Form

---
+++ RegistrationForm/public/ValidForm.js
@@ -0,0 +1,11 @@
+// function validateForm(event) {
+    
+//     if (s1 !== "Valid") alert("Fix Name");
+//     if (s2 !== "Valid") alert("Fix Email");
+//     if (s3 !== "Valid") alert("Fix Contact");
+//     if (s5 !== "Valid DOB") alert("Fix DOB (Must be 18+)");
+
+//     if (s1 !== "Valid" || s2 !== "Valid" || s3 !== "Valid" || s5 !== "Valid DOB") {
+//         event.preventDefault();
+//     }
+// }
